763ad65b9cba94453bf6482a5d064cde86e2109e
Kate & Miki <nathovaekaterina@gmail.com>
2023-05-16 16:24:17 +0300

    Инициированы класс Channel и метод print_info

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,22 @@
+import json
+import os
+import isodate
+
+from googleapiclient.discovery import build
+
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = str(channel_id)
 
-    def print_info(self) -> None:
+    def print_info(self, dict_to_print, indent, ensure_asci) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        return json.dumps(dict_to_print, indent=2, ensure_ascii=False)
+
+
