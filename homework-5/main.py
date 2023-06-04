4232b4ada29572d289ad7ebed84ca10bb8082b75
Nathova <122864131+Nathova@users.noreply.github.com>
2023-06-04 19:10:35 +0300

    Файл без изменений

--- homework-5/main.py
+++ homework-5/main.py
@@ -1,9 +1,9 @@
 import datetime
 
-from src.video import PLVideo
+from src.playlist import PlayList
 
 if __name__ == '__main__':
-    pl = PLVideo('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
+    pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
     assert pl.title == "Moscow Python Meetup №81"
     assert pl.url == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
 
@@ -12,4 +12,5 @@ if __name__ == '__main__':
     assert isinstance(duration, datetime.timedelta)
     assert duration.total_seconds() == 6592.0
 
-    assert pl.show_best_video() == "https://youtu.be/cUGyMzWQcGM"
+    assert pl.show_best_video == "https://youtu.be/cUGyMzWQcGM"
+
