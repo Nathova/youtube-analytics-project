2866c95bf127f5c9b62b7565b8a3554f599fd912
Nathova <122864131+Nathova@users.noreply.github.com>
2023-06-04 14:35:31 +0300

    Внесены исправления

--- homework-5/main.py
+++ homework-5/main.py
@@ -1,9 +1,9 @@
 import datetime
 
-from src.playlist import PlayList
+from src.video import PLVideo
 
 if __name__ == '__main__':
-    pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
+    pl = PLVideo('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
     assert pl.title == "Moscow Python Meetup №81"
     assert pl.url == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
 
